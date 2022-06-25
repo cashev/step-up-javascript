88a7236bc8eef557a0c95afba80abdb6502eebde
cashev <42924021+cashev@users.noreply.github.com>
2022-06-25 16:13:09 +0900

    経過時間をカウントしconsoleに表示

--- 01_stopwatch/main.js
+++ 01_stopwatch/main.js
@@ -2,4 +2,9 @@ var startButton = document.getElementsByClassName('startButton')[0];
 
 startButton.addEventListener('click', function() {
     console.log('start');
+    var seconds = 0;
+    setInterval(function(){
+        seconds++;
+        console.log(seconds);
+    }, 1000);
 });
