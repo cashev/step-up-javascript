cc3d63be18cf740dae4ded7d7a8963fa17116ef1
cashev <42924021+cashev@users.noreply.github.com>
2022-06-25 16:31:08 +0900

    ストップボタンのイベントを実装

--- 01_stopwatch/main.js
+++ 01_stopwatch/main.js
@@ -1,12 +1,19 @@
 var displayElm = document.getElementsByClassName('display')[0];
-var startButton = document.getElementsByClassName('startButton')[0];
+var timer = null;
 
+var startButton = document.getElementsByClassName('startButton')[0];
 startButton.addEventListener('click', function() {
     console.log('start');
     var seconds = 0;
-    setInterval(function(){
+    timer = setInterval(function(){
         seconds++;
         displayElm.innerHTML = seconds;
         console.log(seconds);
     }, 1000);
+});
+
+var stopButton = document.getElementsByClassName('stopButton')[0];
+stopButton.addEventListener('click', function() {
+    clearInterval(timer);
+    timer = null;
 });
