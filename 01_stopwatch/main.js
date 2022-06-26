5f05e89006c1dd4e681bd24909293a01cf3f1a89
cashev <42924021+cashev@users.noreply.github.com>
2022-06-26 11:34:53 +0900

    ストップボタンを押したときに"終了"を表示

--- 01_stopwatch/main.js
+++ 01_stopwatch/main.js
@@ -1,4 +1,11 @@
+function addMessage(message) {
+    var messageElm = document.createElement('div');
+    messageElm.innerText = message;
+    logElm.appendChild(messageElm);
+}
+
 var displayElm = document.getElementsByClassName('display')[0];
+var logElm = document.querySelector('.log');
 var timer = null;
 
 var startButton = document.getElementsByClassName('startButton')[0];
@@ -13,11 +20,7 @@ startButton.addEventListener('click', function() {
         displayElm.innerHTML = seconds;
         console.log(seconds);
     }, 1000);
-    var message = "開始";
-    var messageElm = document.createElement('div');
-    messageElm.innerText = message;
-    var logElm = document.querySelector('.log');
-    logElm.appendChild(messageElm);
+    addMessage('開始');
 });
 
 var stopButton = document.getElementsByClassName('stopButton')[0];
@@ -27,4 +30,5 @@ stopButton.addEventListener('click', function() {
     }
     clearInterval(timer);
     timer = null;
+    addMessage('終了');
 });
