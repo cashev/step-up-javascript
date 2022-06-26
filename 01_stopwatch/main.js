5400874043a7abb322784188fec1e4124414cc05
cashev <42924021+cashev@users.noreply.github.com>
2022-06-26 15:03:28 +0900

    テンプレートリテラルへ変更

--- 01_stopwatch/main.js
+++ 01_stopwatch/main.js
@@ -41,7 +41,7 @@ function stopWatch(options) {
     function addMessage(message) {
         const messageElm = document.createElement('div');
         const now = new Date();
-        messageElm.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒' + message;
+        messageElm.innerText = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
         messageElm.classList = ['message'];
         logElm.appendChild(messageElm);
     }
