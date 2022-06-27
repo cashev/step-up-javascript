f649769360577ceab817488cdb344813ee589e23
cashev <42924021+cashev@users.noreply.github.com>
2022-06-27 23:36:17 +0900

    StopWatch関数をclassへ変更

--- 01_stopwatch/main.js
+++ 01_stopwatch/main.js
@@ -1,49 +1,55 @@
-function stopWatch(options = {}) {
-    // 見た目
-    let {color, backgroundColor} = options;
-    color = color || 'lightblue';
-    backgroundColor = backgroundColor || 'black';
-    const displayElm = document.getElementsByClassName('display')[0];
-    displayElm.style.color = color;
-    displayElm.style.backgroundColor = backgroundColor;
-    const logElm = document.querySelector('.log');
-    // タイマーを保持する
-    let timer = null;
+class StopWatch {
+    constructor(options = {}) {
+        this.options = options;
+    }
 
-    // 開始ボタン
-    const startButton = document.getElementsByClassName('startButton')[0];
-    startButton.addEventListener('click', () => {
-        if (timer !== null) {
-            return;
-        }
-        console.log('start');
-        let seconds = 0;
-        timer = setInterval(function(){
-            seconds++;
-            displayElm.innerHTML = seconds;
-            console.log(seconds);
-        }, 1000);
-        addMessage('開始');
-    });
+    init() {
+        // 見た目
+        let {color, backgroundColor} = this.options;
+        color = color || 'lightblue';
+        backgroundColor = backgroundColor || 'black';
+        const displayElm = document.getElementsByClassName('display')[0];
+        displayElm.style.color = color;
+        displayElm.style.backgroundColor = backgroundColor;
+        this.logElm = document.querySelector('.log');
+        // タイマーを保持する
+        let timer = null;
 
-    // 終了ボタン
-    const stopButton = document.getElementsByClassName('stopButton')[0];
-    stopButton.addEventListener('click', () => {
-        if (timer === null) {
-            return;
-        }
-        clearInterval(timer);
-        timer = null;
-        addMessage('終了');
-    });
+        // 開始ボタン
+        const startButton = document.getElementsByClassName('startButton')[0];
+        startButton.addEventListener('click', () => {
+            if (timer !== null) {
+                return;
+            }
+            console.log('start');
+            let seconds = 0;
+            timer = setInterval(function(){
+                seconds++;
+                displayElm.innerHTML = seconds;
+                console.log(seconds);
+            }, 1000);
+            this.addMessage('開始');
+        });
+
+        // 終了ボタン
+        const stopButton = document.getElementsByClassName('stopButton')[0];
+        stopButton.addEventListener('click', () => {
+            if (timer === null) {
+                return;
+            }
+            clearInterval(timer);
+            timer = null;
+            this.addMessage('終了');
+        });
+    }
 
     // ログを表示する
-    const addMessage = (message) => {
+    addMessage(message) {
         const messageElm = document.createElement('div');
         const now = new Date();
         messageElm.innerText = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
         messageElm.classList = ['message'];
-        logElm.appendChild(messageElm);
+        this.logElm.appendChild(messageElm);
     }
 }
 
@@ -51,5 +57,5 @@ const options = {
     color: 'limegreen',
     backgroundColor: '#333',
 };
-stopWatch(options);
-// stopWatch();
+const stopWatch = new StopWatch(options);
+stopWatch.init();
